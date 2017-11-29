122761af1e5eea1ea91e7f574666c7909d4d7257
oipanuwatio <oipanuwatio@gmail.com>
2017-11-30 02:02:55 +0700

    Me.

--- index.php
+++ index.php
@@ -15,38 +15,21 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 
-        // Line API send a lot of event type, we interested in message only.
-		if ($event['type'] == 'message') {
-            switch($event['message']['type']) {
 
-                case 'text':
-           $replyToken = $event['replyToken'];
-           $ask = $event['message']['text'];
-           switch(strtolower($ask)) {
-               case 'm':
-                   $respMessage = 'What sup man. Go away!';
-                   break;
-               case 'f':
-                   $respMessage = 'Love you lady.';
-                   break;
-               default:
-                   $respMessage = 'What is your sex? M or F';
-                   break;
-           }
-                   break;
+	// Loop through each event
+	foreach ($events['events'] as $event) {
 
-                   case 'image':
-                   $originalContentUrl = 'https://cdn.shopify.com/s/files/1/1217/6360/products/Shinkansen_Tokaido_ShinFuji_001_1e44e709-ea47-41ac-91e4-89b2b5eb193a_grande.jpg?v=1489641827';
-    $previewImageUrl = 'https://cdn.shopify.com/s/files/1/1217/6360/products/Shinkansen_Tokaido_ShinFuji_001_1e44e709-ea47-41ac-91e4-89b2b5eb193a_grande.jpg?v=1489641827';
+        // Get replyToken
+        $replyToken = $event['replyToken'];
+        // Image
+        $originalContentUrl = 'https://cdn.shopify.com/s/files/1/1217/6360/products/Shinkansen_Tokaido_ShinFuji_001_1e44e709-ea47-41ac-91e4-89b2b5eb193a_grande.jpg?v=1489641827';
+        $previewImageUrl = 'https://cdn.shopify.com/s/files/1/1217/6360/products/Shinkansen_Tokaido_ShinFuji_001_1e44e709-ea47-41ac-91e4-89b2b5eb193a_grande.jpg?v=1489641827';
+        $httpClient = new CurlHTTPClient($channel_token);
+        $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+        $textMessageBuilder = new ImageMessageBuilder($originalContentUrl, $previewImageUrl);
+        $response = $bot->replyMessage($replyToken, $textMessageBuilder);
 
-                       break;
-            }
-            $httpClient = new CurlHTTPClient($channel_token);
-            $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-            $textMessageBuilder = new TextMessageBuilder($respMessage);
-            $response = $bot->replyMessage($replyToken, $textMessageBuilder);
-
-		}
+	}
 	}
 }
 echo "OK";
