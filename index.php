7e2d5b1c59dd441a957184f3552ac0bf2a38d0f4
oipanuwatio <oipanuwatio@gmail.com>
2017-11-30 00:40:34 +0700

    Me.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once('./vendor/autoload.php');
 // Namespace
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
-use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
 // Token
 $channel_token = '/avi4EmY8kTPWXO+awWk+ztd3I2HD9BAS/WHgY/GyKJnpmJ/M4lHlBxWFNr8V5x+IUV+4oEDPJOj02U9pGP19daIqHwkmWLyOOnElf0CrNzGgGTQOIkxjf00q2zQU2wH8kstcGc9yr17a6NqkTcofwdB04t89/1O/w1cDnyilFU=';
 $channel_secret = 'b7ec1714f2db948a7ff3cfbe7e5164c2';
@@ -16,24 +16,12 @@ if (!is_null($events['events'])) {
 
         // Get replyToken
         $replyToken = $event['replyToken'];
-        $ask = $event['message']['text'];
-        switch(strtolower($ask)) {
-            case '1':
-                $respMessage = 'ตอนนี้ lineBot สามารถถามชื่อ,idผู้ใช้งาน,คำถามทั่วๆไป....';
-                break;
-            case 'f':
-                $respMessage = "สวัสดี ID คุณคือ ".$events['events'][0]['source']['userId'];
-                break;
-            default:
-                $respMessage = 'lineBot ยังไม่ได้เรียนรู้ค้าบ พิม 1 เพื่อเรียนรู้';
-                break;
-        }
-        $originalContentUrl = 'http://static.weloveshopping.com/shop/client/000049/krichhouse/AIM270011.jpg';
-        $previewImageUrl = 'http://static.weloveshopping.com/shop/client/000049/krichhouse/AIM270011.jpg';
-
+        // Image
+        $originalContentUrl = 'https://cdn.shopify.com/s/files/1/1217/6360/products/Shinkansen_Tokaido_ShinFuji_001_1e44e709-ea47-41ac-91e4-89b2b5eb193a_grande.jpg?v=1489641827';
+        $previewImageUrl = 'https://cdn.shopify.com/s/files/1/1217/6360/products/Shinkansen_Tokaido_ShinFuji_001_1e44e709-ea47-41ac-91e4-89b2b5eb193a_grande.jpg?v=1489641827';
         $httpClient = new CurlHTTPClient($channel_token);
         $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-        $textMessageBuilder = new TextMessageBuilder($respMessage,$originalContentUrl, $previewImageUrl);
+        $textMessageBuilder = new ImageMessageBuilder($originalContentUrl, $previewImageUrl);
         $response = $bot->replyMessage($replyToken, $textMessageBuilder);
 
 	}
