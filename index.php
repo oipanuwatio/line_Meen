cb7171cfee524afe1d90548a0311eaf62004d15f
oipanuwatio <oipanuwatio@gmail.com>
2017-11-30 00:23:33 +0700

    Me.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once('./vendor/autoload.php');
 // Namespace
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
-use \LINE\LINEBot\MessageBuilder\VideoMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 // Token
 $channel_token = '/avi4EmY8kTPWXO+awWk+ztd3I2HD9BAS/WHgY/GyKJnpmJ/M4lHlBxWFNr8V5x+IUV+4oEDPJOj02U9pGP19daIqHwkmWLyOOnElf0CrNzGgGTQOIkxjf00q2zQU2wH8kstcGc9yr17a6NqkTcofwdB04t89/1O/w1cDnyilFU=';
 $channel_secret = 'b7ec1714f2db948a7ff3cfbe7e5164c2';
@@ -16,12 +16,21 @@ if (!is_null($events['events'])) {
 
         // Get replyToken
         $replyToken = $event['replyToken'];
-        // Video
-        $originalContentUrl = 'https://www.select2web.com.com/the-fuji.mp4';
-        $previewImageUrl = 'http://static.weloveshopping.com/shop/client/000049/krichhouse/AIM270011.jpg';
+        $ask = $event['message']['text'];
+        switch(strtolower($ask)) {
+            case '1':
+                $respMessage = 'ตอนนี้ lineBot สามารถถามชื่อ,idผู้ใช้งาน,คำถามทั่วๆไป....';
+                break;
+            case 'f':
+                $respMessage = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
+                break;
+            default:
+                $respMessage = 'lineBot ยังไม่ได้เรียนรู้ค้าบ พิม 1 เพื่อเรียนรู้';
+                break;
+        }
         $httpClient = new CurlHTTPClient($channel_token);
         $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-        $textMessageBuilder = new VideoMessageBuilder($originalContentUrl, $previewImageUrl);
+        $textMessageBuilder = new TextMessageBuilder($respMessage);
         $response = $bot->replyMessage($replyToken, $textMessageBuilder);
 
 	}
