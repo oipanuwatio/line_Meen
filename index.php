6f23bc81ec0004c837cd0d4b741970101c54506a
oipanuwatio <oipanuwatio@gmail.com>
2017-11-30 00:49:50 +0700

    Me.

--- index.php
+++ index.php
@@ -1,32 +1,9 @@
 <?php
-/*
-{
-   "events":[
-      {
-         "replyToken":"nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
-         "type":"message",
-         "timestamp":1462629479859,
-         "source":{
-            "type":"user",
-            "userId":"U206d25c2ea6bd87c17655609a1c37cb8"
-         },
-         "message":{
-            "id":"325708",
-            "type":"location",
-            "title":"my location",
-            "address":"〒150-0002 東京都渋谷区渋谷２丁目２１−１",
-            "latitude":35.65910807942215,
-            "longitude":139.70372892916203
-         }
-      }
-   ]
-}
- */
 require_once('./vendor/autoload.php');
 // Namespace
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
-use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\LocationMessageBuilder;
 // Token
 $channel_token = '/avi4EmY8kTPWXO+awWk+ztd3I2HD9BAS/WHgY/GyKJnpmJ/M4lHlBxWFNr8V5x+IUV+4oEDPJOj02U9pGP19daIqHwkmWLyOOnElf0CrNzGgGTQOIkxjf00q2zQU2wH8kstcGc9yr17a6NqkTcofwdB04t89/1O/w1cDnyilFU=';
 $channel_secret = 'b7ec1714f2db948a7ff3cfbe7e5164c2';
@@ -37,29 +14,18 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 
-        // Line API send a lot of event type, we interested in message only.
-		if ($event['type'] == 'message') {
-            // Get replyToken
-            $replyToken = $event['replyToken'];
+        // Get replyToken
+        $replyToken = $event['replyToken'];
+        // Location
+        $title = 'I am here';
+        $address = 'Fitness 7 Ratchada';
+        $latitude = '13.7743425';
+        $longitude = '100.5680782';
+        $httpClient = new CurlHTTPClient($channel_token);
+        $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+        $textMessageBuilder = new LocationMessageBuilder($title, $address, $latitude, $longitude);
+        $response = $bot->replyMessage($replyToken, $textMessageBuilder);
 
-            switch($event['message']['type']) {
-
-                case 'location':
-                    $address = $event['message']['address'];
-                    // Reply message
-                    $respMessage = 'Hello, your address is '. $address;
-
-                    break;
-                default:
-                    // Reply message
-                    $respMessage = 'Please send location only';
-                    break;
-            }
-            $httpClient = new CurlHTTPClient($channel_token);
-            $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-            $textMessageBuilder = new TextMessageBuilder($respMessage);
-            $response = $bot->replyMessage($replyToken, $textMessageBuilder);
-		}
 	}
 }
 echo "OK";
