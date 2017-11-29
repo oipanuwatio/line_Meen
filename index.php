54661ea14871ecb76322c16061a16a3abe8c8ac6
oipanuwatio <oipanuwatio@gmail.com>
2017-11-30 00:08:30 +0700

    Me.

--- index.php
+++ index.php
@@ -1,42 +1,29 @@
 <?php
-
 require_once('./vendor/autoload.php');
 // Namespace
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
-use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\VideoMessageBuilder;
 // Token
 $channel_token = '/avi4EmY8kTPWXO+awWk+ztd3I2HD9BAS/WHgY/GyKJnpmJ/M4lHlBxWFNr8V5x+IUV+4oEDPJOj02U9pGP19daIqHwkmWLyOOnElf0CrNzGgGTQOIkxjf00q2zQU2wH8kstcGc9yr17a6NqkTcofwdB04t89/1O/w1cDnyilFU=';
 $channel_secret = 'b7ec1714f2db948a7ff3cfbe7e5164c2';
 // Get message from Line API
 $content = file_get_contents('php://input');
-$arrJson = json_decode($content, true);
-if (!is_null($arrJson['events'])) {
+$events = json_decode($content, true);
+if (!is_null($events['events'])) {
 	// Loop through each event
-	foreach ($arrJson['events'] as $arrJson) {
+	foreach ($events['events'] as $event) {
 
-        // Line API send a lot of event type, we interested in message only.
-        if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
-      $arrPostData = array();
-      $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-      $arrPostData['messages'][0]['type'] = "text";
-      $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
-    }else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
-      $arrPostData = array();
-      $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-      $arrPostData['messages'][0]['type'] = "text";
-      $arrPostData['messages'][0]['text'] = "ฉันยังไม่มีชื่อนะ";
-    }else if($arrJson['events'][0]['message']['text'] == "ทำอะไรได้บ้าง"){
-      $arrPostData = array();
-      $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-      $arrPostData['messages'][0]['type'] = "text";
-      $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
-    }else{
-      $arrPostData = array();
-      $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-      $arrPostData['messages'][0]['type'] = "text";
-      $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
-    }
+        // Get replyToken
+        $replyToken = $event['replyToken'];
+        // Video
+        $originalContentUrl = 'https://www.select2web.com.com/the-fuji.mp4';
+        $previewImageUrl = 'https://www.select2web.com.com/the-fuji.jpg';
+        $httpClient = new CurlHTTPClient($channel_token);
+        $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+        $textMessageBuilder = new VideoMessageBuilder($originalContentUrl, $previewImageUrl);
+        $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+
 	}
 }
 echo "OK";
