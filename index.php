706e7e1b066d5babdc16ab1509ef8e98cb89b8f5
oipanuwatio <oipanuwatio@gmail.com>
2017-11-30 01:19:14 +0700

    Me.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once('./vendor/autoload.php');
 // Namespace
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
-use \LINE\LINEBot\MessageBuilder\LocationMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 // Token
 $channel_token = '/avi4EmY8kTPWXO+awWk+ztd3I2HD9BAS/WHgY/GyKJnpmJ/M4lHlBxWFNr8V5x+IUV+4oEDPJOj02U9pGP19daIqHwkmWLyOOnElf0CrNzGgGTQOIkxjf00q2zQU2wH8kstcGc9yr17a6NqkTcofwdB04t89/1O/w1cDnyilFU=';
 $channel_secret = 'b7ec1714f2db948a7ff3cfbe7e5164c2';
@@ -14,19 +14,32 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 
-        // Get replyToken
-        $replyToken = $event['replyToken'];
-        // Location
-        $title = 'I am here';
-        $address = 'Fitness 7 Ratchada';
-        $latitude = '13.7743425';
-        $longitude = '100.5680782';
-        $httpClient = new CurlHTTPClient($channel_token);
-        $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-        if ($event['type'] == 'text') {
-        $textMessageBuilder = new LocationMessageBuilder($title, $address, $latitude, $longitude);}
-        $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+        // Line API send a lot of event type, we interested in message only.
+		if ($event['type'] == 'message') {
+            switch($event['message']['type']) {
 
+                case 'text':
+                $replyToken = $event['replyToken'];
+    $ask = $event['message']['text'];
+    switch(strtolower($ask)) {
+        case 'm':
+            $respMessage = 'What sup man. Go away!';
+            break;
+        case 'f':
+            $respMessage = 'Love you lady.';
+            break;
+        default:
+            $respMessage = 'What is your sex? M or F';
+            break;
+    }
+    $httpClient = new CurlHTTPClient($channel_token);
+    $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+    $textMessageBuilder = new TextMessageBuilder($respMessage);
+    $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+
+                    break;
+            }
+		}
 	}
 }
 echo "OK";
