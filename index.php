a0209e1874908bc454f2566896e3ec7d28471e44
oipanuwatio <oipanuwatio@gmail.com>
2017-11-30 02:13:16 +0700

    Me.

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
-
 require_once('./vendor/autoload.php');
 // Namespace
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 use \LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\LocationMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\VideoMessageBuilder;
 // Token
 $channel_token = '/avi4EmY8kTPWXO+awWk+ztd3I2HD9BAS/WHgY/GyKJnpmJ/M4lHlBxWFNr8V5x+IUV+4oEDPJOj02U9pGP19daIqHwkmWLyOOnElf0CrNzGgGTQOIkxjf00q2zQU2wH8kstcGc9yr17a6NqkTcofwdB04t89/1O/w1cDnyilFU=';
 $channel_secret = 'b7ec1714f2db948a7ff3cfbe7e5164c2';
@@ -16,21 +18,36 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 
+        // Line API send a lot of event type, we interested in message only.
+		if ($event['type'] == 'message') {
+            switch($event['message']['type']) {
+              $replyToken = $event['replyToken'];
+              $ask = $event['message']['text'];
 
-	// Loop through each event
-	foreach ($events['events'] as $event) {
+                case 'text':
+                switch(strtolower($ask)) {
+                    case 'm':
+                        $respMessage = 'What sup man. Go away!';
+                        break;
+                    case 'f':
+                        $respMessage = 'Love you lady.';
+                        break;
+                    default:
+                        $respMessage = 'What is your sex? M or F';
+                        break;
+                }
 
-        // Get replyToken
-        $replyToken = $event['replyToken'];
-        // Image
-        $originalContentUrl = 'https://cdn.shopify.com/s/files/1/1217/6360/products/Shinkansen_Tokaido_ShinFuji_001_1e44e709-ea47-41ac-91e4-89b2b5eb193a_grande.jpg?v=1489641827';
-        $previewImageUrl = 'https://cdn.shopify.com/s/files/1/1217/6360/products/Shinkansen_Tokaido_ShinFuji_001_1e44e709-ea47-41ac-91e4-89b2b5eb193a_grande.jpg?v=1489641827';
+                    break;
+
+
+
+
+            }
+		}
         $httpClient = new CurlHTTPClient($channel_token);
         $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-        $textMessageBuilder = new ImageMessageBuilder($originalContentUrl, $previewImageUrl);
+        $textMessageBuilder = new TextMessageBuilder($respMessage);
         $response = $bot->replyMessage($replyToken, $textMessageBuilder);
-
-	}
 	}
 }
 echo "OK";
