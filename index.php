19a1c0105bfa48e4a24b224053b185e25c347970
oipanuwatio <oipanuwatio@gmail.com>
2017-11-30 02:20:47 +0700

    Me.

--- index.php
+++ index.php
@@ -18,20 +18,27 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 
-    // Get replyToken
-            $replyToken = $event['replyToken'];
-            $ask = $event['message']['text'];
-            switch(strtolower($ask)) {
-                case 'm':
-                    $respMessage = 'What sup man. Go away!';
-                    break;
-                case 'f':
-                    $respMessage = 'Love you lady.';
-                    break;
-                default:
-                    $respMessage = 'What is your sex? M or F';
-                    break;
-            }
+    switch($event['message']['type']) {
+
+                    case 'text':
+                    // Get replyToken
+                            $replyToken = $event['replyToken'];
+                            $ask = $event['message']['text'];
+                            switch(strtolower($ask)) {
+                                case 'm':
+                                    $respMessage = 'What sup man. Go away!';
+                                    break;
+                                case 'f':
+                                    $respMessage = 'Love you lady.';
+                                    break;
+                                default:
+                                    $respMessage = 'What is your sex? m or f';
+                                    break;
+                            }
+
+                        break;
+                }
+
             $httpClient = new CurlHTTPClient($channel_token);
             $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
             $textMessageBuilder = new TextMessageBuilder($respMessage);
